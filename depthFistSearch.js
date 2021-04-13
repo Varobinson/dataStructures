9531303704588d69fa82826ffcd6621d9f59ea9c
Vincent Robinson <59773500+Varobinson@users.noreply.github.com>
2021-04-13 12:22:35 -0400

    depth first inOrder

---
+++ depthFistSearch.js
@@ -0,0 +1,61 @@
+//Pre-order
+
+
+function PreOrder(root){
+    let data = [],
+        current = root;
+    
+    function traverse(node){
+        data.push(node)
+        if(node.left) traverse(node.left);
+        if(node.right) traverse(node.right);
+        }
+
+    traverse(current);
+    return data;
+}
+
+//Post order
+
+function postOrder(root){
+    let data = [],
+        current = root;
+    
+    function traverse(node){
+        if(node.left) traverse(node.left);
+        if(node.right) traverse(node.right);
+        data.push(node)
+    }
+    traverse(current);
+    return data;
+}
+//es6
+
+// const traverse = (node , data) =>{
+//     if(node.left) traverse(node.left);
+//     if(node.right) traverse(node.right);
+//     data.push(node);
+//     return data;
+// }
+
+// const PreOrder = (root) =>{
+//     const data = [],
+//         current = root;
+//     const traversedData = traverse(current, data);
+//     return traversedData;
+// }
+
+// IN Order
+
+function inOrder(root){
+    const data = [],
+        current = root;
+    function traverse(node){
+        if(node.left) traverse(node.left);
+        data.push(node);
+        if(node.right) traverse(node.right);
+    }
+    traverse(current);
+    return data;
+}
+  
