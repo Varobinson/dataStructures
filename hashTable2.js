c832d8e3e0c1ab7853949d4784d2f4b3535e8822
Vincent Robinson <59773500+Varobinson@users.noreply.github.com>
2021-04-20 15:40:33 -0400

    hashtable using object

--- hashTable2.js
+++ hashTable2.js
@@ -27,5 +27,62 @@ const myTable = new HashTable();
 myTable.setItem('name', 'bob');
 myTable.getItem('name');
 
-console.log(myTable.getItem('name'));
+// console.log(myTable.getItem('name'));
 
+class Hash2{
+    constructor(size){
+        this.size = size;
+        this.values = {};
+    }
+
+    hashKey(key){
+        const hash = key.toString();
+        let sum = 31;
+        
+        for(let i = 0; i < hash.length; i++){
+            sum += hash.charCodeAt(i);
+        }
+        return sum % this.size;
+    };
+
+    addHash(key, value){
+        const hashedKey = this.hashKey(key);
+        if(!this.values.hasOwnProperty(hashedKey)){
+            this.values[hashedKey] = [];
+            this.values[hashedKey][key] = value;
+        }
+        else if(!this.values[hashedKey].hasOwnProperty(key)){
+            this.values[hashedKey][key] = value;
+        }else console.log(`${key} already exists`);
+    };
+
+    getHash(key){
+        const hash = this.hashKey(key);
+        if(this.values[hash]){
+        return this.values[hash][key]
+        }else{
+            return undefined;
+        }
+    };
+
+    removeHash(key){
+        const hash = this.hashKey(key);
+        if(!this.values.hasOwnProperty(hash) || !this.values[hash][key]){
+            return undefined;
+        }else{
+            delete this.values[hash][key];
+        }
+    };
+}
+
+const myHash = new Hash2(11);
+myHash.addHash('item1' , 'value1');
+myHash.addHash('ite2m' , 'vaule2');
+myHash.addHash('item2' , 'value2');
+myHash.addHash('tiem2' , 'value2');
+
+console.log(myHash)
+console.log(myHash.getHash('item2'))
+myHash.removeHash('item2')
+console.log(myHash)
+console.log(myHash.getHash('item2'))
