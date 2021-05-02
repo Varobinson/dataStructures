692bd10a8f57c134590daf6f2b4cb2f037416eac
Vincent Robinson <59773500+Varobinson@users.noreply.github.com>
2021-05-02 13:00:39 -0400

    containsDuplicates

---
+++ problems/containsDuplicates.js
@@ -0,0 +1,8 @@
+var containsDuplicate = function(nums) {
+    const hash ={};
+    for(let num of nums){
+        if(hash[num]){
+            return true
+        }else hash[num] = true;
+    }return false;
+};
