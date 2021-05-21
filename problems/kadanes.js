ba50a8925fff0c3f134e6f7158cbb7e4d0bd1ec4
Vincent Robinson <59773500+Varobinson@users.noreply.github.com>
2021-05-20 22:03:10 -0400

    kadanes

---
+++ problems/kadanes.js
@@ -0,0 +1,25 @@
+const array = [3,5,-9,1,3,-2,3,4,7,2,-9,6,3,1,-5,4];
+
+function subSum(arr){
+    let max = -Infinity;
+
+    for(let i = 0; i < arr.length; i++){
+        let total;
+        for (let j = 0; i < arr.length; j++){
+            if(total > max) max = total;
+            total += arr[j];
+            
+        }
+    }return max;
+}
+
+function kadanesAlgorithm(array) {
+    let currentMax = array[0], 
+              overallMax = array[0];
+      for(let i = 1 ; i < array.length; i++){
+              const num = array[i];
+              currentMax = Math.max(num, currentMax + num);
+              overallMax = Math.max(overallMax, currentMax);
+      }
+      return overallMax;
+  }
